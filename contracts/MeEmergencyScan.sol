7134ca3c9d3d350c3980b783f6dc6a9d752bc825
Victor Lopez <vmlopez@slashweb.com.mx>
2023-06-24 19:17:22 -0400

    Adding JSON ABI to use on BOS

--- contracts/MeEmergencyScan.sol
+++ contracts/MeEmergencyScan.sol
@@ -5,8 +5,13 @@ pragma solidity >=0.8.2 <0.9.0;
 contract MeEmergencyScan {
 
     struct ParamedicData {
+        string name;
         string date;
-        string description;
+        string vitalSigns;
+        string status;
+        string[] drugs;
+        string[] procedures;
+        string lastMeal;
     }
 
     struct UserRecord {
@@ -17,8 +22,19 @@ contract MeEmergencyScan {
         string[] alergies;
         string[] cronichConditions;
         string[] surgeries;
-        string[] recipes;
+        string[] drugs;
     }
+
+    struct MedicalHistory {
+        address patien;
+        string[] chronicDiseases;
+        string[] surgeries;
+        string[] pathologicalHistory;
+        string[] nonPathologicalHistory;
+        string[] paternalFamilyInheritedPathologies;
+        string[] martenalFamilyInheritedPathologies;
+    }
+
 
     struct User {
         address wallet;
@@ -26,19 +42,47 @@ contract MeEmergencyScan {
         UserRecord record;
     }
 
-    User[] public Users;
 
+    struct Doctor {
+        address wallet;
+        string name;
+        bool isActive;
+        // TODO Rating and another kind of stuffs in
+        // this structure
+    }
+
+
+    // General users for the plattform
+    User[] public Users;
+    MedicalHistory[] public MedicalHistories;
+
+
+    // Basic data for the users
     UserRecord public newUserRecord;
 
+    // Doctors for the contract
+    Doctor[] Doctors;
 
     function compareStrings(string memory a, string memory b) public pure returns (bool) {
         return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
     }
 
+    // Get User Medical History
+    function getUserHistory(address wallet) public view returns (MedicalHistory memory) {
+        for (uint256 i=0; i < MedicalHistories.length; i++) {
+            if (MedicalHistories[i].patien == wallet) {
+                return MedicalHistories[i];
+            }
+        }
+
+        MedicalHistory memory empty;
+        return empty;
+    }
+
     // Get Actual profile of the user
-    function getUserRecord() public view returns (User memory) {
-        for (uint256 i = 0; i < Users.length; i++) {
-            if (Users[i].wallet == msg.sender) {
+    function getUserRecord(address wallet) public view returns (User memory) {
+        for (uint256 i=0; i < Users.length; i++) {
+            if (Users[i].wallet == wallet) {
                 return Users[i];
             }
         }
@@ -46,6 +90,12 @@ contract MeEmergencyScan {
         User memory empty;
         return empty;
     }
+
+    // Get All users records just for testing purpose
+    function getAllUserRecords() public view returns (User[] memory) {
+        return Users;
+    }
+
 
     // Create basic new User Profile
     function createUserProfile(
@@ -56,19 +106,93 @@ contract MeEmergencyScan {
         string[] memory alergies,
         string[] memory cronichConditions,
         string[] memory surgeries,
-        string[] memory recipes
+        string[] memory drugs
     ) public returns (bool) {
 
-        for (uint256 i = 0; i < Users.length; i++) {
+        for (uint256 i=0; i < Users.length; i++) {
             if (Users[i].wallet == msg.sender) {
                 return false;
             }
         }
 
-        newUserRecord = UserRecord(name, bloodType, emergencyPhone, emergencyName, alergies, cronichConditions, surgeries, recipes);
-        Users.push(User(msg.sender, 'user', newUserRecord));
+        newUserRecord = UserRecord(
+            name,
+            bloodType,
+            emergencyPhone,
+            emergencyName,
+            alergies,
+            cronichConditions,
+            surgeries,
+            drugs
+        );
+
+        Users.push( User(msg.sender, 'user', newUserRecord));
+        createMedicalHistory(msg.sender);
 
         return true;
     }
 
+    // Create basic empty Medical History
+    function createMedicalHistory(address patien) private returns (bool) {
+
+        MedicalHistory memory medicalHistory;
+        medicalHistory.patien = patien;
+        MedicalHistories.push(medicalHistory);
+
+        return true;
+    }
+
+    function updatePropInMedicalHistory(string memory prop, string memory value, uint256 index) private returns (bool) {
+        if (compareStrings(prop, "chronicDiseases")) {
+            MedicalHistories[index].chronicDiseases.push(value);
+        }
+        if (compareStrings(prop, "surgeries")) {
+            MedicalHistories[index].surgeries.push(value);
+        }
+        if (compareStrings(prop, "surgeries")) {
+            MedicalHistories[index].surgeries.push(value);
+        }
+        if (compareStrings(prop, "pathologicalHistory")) {
+            MedicalHistories[index].pathologicalHistory.push(value);
+        }
+        if (compareStrings(prop, "nonPathologicalHistory")) {
+            MedicalHistories[index].nonPathologicalHistory.push(value);
+        }
+        if (compareStrings(prop, "paternalFamilyInheritedPathologies")) {
+            MedicalHistories[index].paternalFamilyInheritedPathologies.push(value);
+        }
+        if (compareStrings(prop, "martenalFamilyInheritedPathologies")) {
+            MedicalHistories[index].martenalFamilyInheritedPathologies.push(value);
+        }
+        return true;
+    }
+
+    function updateMedicalHistory(address patien, string memory prop, string memory value) public returns (bool) {
+        bool doctorIsActive = false;
+
+        for (uint256 i=0; i < Doctors.length; i++) {
+            if (Doctors[i].wallet == msg.sender) {
+                doctorIsActive = true;
+            }
+        }
+
+        if (!doctorIsActive) {
+            return false;
+        }
+
+        for (uint256 i=0; i < MedicalHistories.length; i++) {
+            if (MedicalHistories[i].patien == patien) {
+                updatePropInMedicalHistory(prop, value, i);
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+
+    function createDoctor(string memory name) public returns (bool) {
+        Doctors.push( Doctor(msg.sender, name, true));
+        return true;
+    }
 }
